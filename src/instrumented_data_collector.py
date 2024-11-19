59b109b531b4cf6582ef5c4889c9fbced1df964b
David de Almeida Cunha <daviid.0203@gmail.com>
2024-11-19 14:38:40 -0300

    fix: adding Windows libc search option

--- src/instrumented_data_collector.py
+++ src/instrumented_data_collector.py
@@ -1,14 +1,30 @@
 import os
 import sys
 import io
-from tempfile import TemporaryFile
 import ctypes
+from tempfile import TemporaryFile
+from ctypes.util import find_library
 
+def load_standard_library():
+    if os.name == 'nt':  # Windows
+        return ctypes.CDLL("msvcrt.dll")
+    else: # Linux
+        libc_path = find_library("c")
+        if not libc_path:
+            raise OSError("Could not find the standard C library")
+        return ctypes.CDLL(libc_path)
+
+def load_stdout(libc):
+    if os.name == 'nt':  # Windows
+        stdout = ctypes.c_void_p.in_dll(libc, '_iob')  
+        c_stdout = ctypes.cast(stdout, ctypes.POINTER(ctypes.c_void_p))[1] 
+    else:  # Linux
+        c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
 
 class InstrumentedDataCollector():
     def __init__ (self):
-        self.libc = ctypes.CDLL(None)
-        self.c_stdout = ctypes.c_void_p.in_dll(self.libc, 'stdout')
+        self.libc = load_standard_library()
+        self.c_stdout = load_stdout(self.libc)
         self.tempfile = TemporaryFile(mode='w+')
         self.intercepted_fd = sys.stdout.fileno()
         self.duplicated_stdout = os.dup(self.intercepted_fd)
