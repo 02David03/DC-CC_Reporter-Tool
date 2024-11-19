fac7cdf9c30219ada96525f1a9de0ad67bb61d62
David de Almeida Cunha <daviid.0203@gmail.com>
2024-11-18 22:19:18 -0300

    refactor: Changing coupling view mode in report

--- report/script/coupling_sut_detail.js
+++ report/script/coupling_sut_detail.js
@@ -4,16 +4,19 @@ mountAnalysesGRN0();
 mountInputsTable();
 
 function mountAnalysesGRN0() {
-  let graphData = [0, 0];
+  let graphData = [0, 0, 0];
   for(const key in couplingSUT) {
     const subObj = couplingSUT[key];
-    const hasValues = Object.keys(subObj).some(
-      subKey => subObj[subKey].length > 0
-    );
+    const hasValues = Object.keys(subObj).length > 1; 
+    console.log(hasValues)
     if(hasValues) {
       graphData[0]++
-    } else{
-      graphData[1]++
+    } else {
+      if(subObj['analysed']) {
+        graphData[2]++
+      } else {
+        graphData[1]++
+      }
     }
   }
   mountCouplingDonutGraph("GRN0-donut-graph", "GRN0-tooltip", graphData);
@@ -25,36 +28,45 @@ function mountInputsTable() {
   insertInputsCelCol();
   let outputArr = [];
   Object.values(couplingSUT).forEach(value => {
-    outputArr = [...new Set([...(Object.keys(value)), ...outputArr])];
+    outputArr = [...new Set([...(Object.keys(value)), ...outputArr])].filter(key => key !== 'analysed');
   });
   outputArr = outputArr.sort();
   insertOutputsSubHeadCelArray(outputArr);
   insertOutputsTableCelArray(outputArr);
   
   //Defining width pattern to all cels
-  let cels = $(".subhead-table-cel, .table-cel").not("#entries-col *");
+  let celsToWidth = $(".subhead-table-cel, .table-cel").not("#entries-col *");
 
+  let maxHeight = 0;
   let maxWidth = 54;
-  cels.each(function() {
-      let elementWidth = $(this).outerWidth();
-      if (elementWidth > maxWidth) {
-          maxWidth = elementWidth;
-      }
-    });
+  celsToWidth.each(function() {
+    let elementWidth = $(this).outerWidth();
+    if (elementWidth > maxWidth) {
+        maxWidth = elementWidth;
+    }
+  });
 
-    cels.css("width", maxWidth);
+  $(".table-cel").each(function() {
+    let elementHeight = $(this).outerHeight();
+    if (elementHeight > maxHeight) {
+      maxHeight = elementHeight;
+    }
+  });
+    
+  celsToWidth.css("width", maxWidth + 5);
+  $(".table-cel").css("height", maxHeight);
 }
 
 function insertInputsCelCol() {
   Object.keys(couplingSUT).forEach(key => {
-    $(`#entries-col`).append(`<div class='table-cel ${key % 2 === 0 ? 'dark' : ''}'>#` + key + "</div>");
+    $(`#entries-col`).append(`<div class='table-cel ${key % 2 === 0 ? 'dark' : ''}'>` + key + "</div>");
   });
 }
 
 function insertOutputsSubHeadCelArray(arr) {
   let arrCels = "<div class='d-flex align-items-center'>";
   for (let i = 0; i < arr.length; i++) {
-    arrCels += "<div class='subhead-table-cel w-100'> #" + arr[i] + "</div>"
+    arrCels += "<div class='subhead-table-cel w-100'>" + arr[i] + "</div>"
   }
   arrCels += '</div>';
   $(`#output-col`).append(arrCels);
@@ -65,7 +77,9 @@ function insertOutputsTableCelArray(outputs) {
     let arrCels = "<div class='d-flex align-items-center'>";
     for (let i = 0; i < outputs.length; i++) {
       if(entry[1][outputs[i]]) {
-        arrCels += `<div class="table-cel coupled">` + entry[1][outputs[i]] + "</div>";
+        arrCels += `<div class="table-cel coupled">` + entry[1][outputs[i]].map(testes => {
+          return 'teste ' + testes[0] +' <=> ' + 'teste ' + testes[1];
+        }).join('<br>') + "</div>";
       } else {
         arrCels += `<div class="table-cel ${entry[0] % 2 === 0 ? 'dark' : ''}"> # </div>`;
       }
--- report/script/graphs.js
+++ report/script/graphs.js
@@ -1,10 +1,12 @@
 
-function mountCouplingDonutGraph(canva_id, tooltip_id, data) {
+function mountCouplingDonutGraph(canva_id, tooltip_id, data, is_component=false) {
   const canvas = document.getElementById(canva_id);
   const ctx = canvas.getContext("2d");
 
-  const labels = ["Entradas acopladas", "Entradas não identificaveis"];
-  const colors = ["#0a1a5c", "#6E6E6E"];
+  const labels = !is_component ? 
+    ["Entradas acopladas:", "Entradas não analisadas:", "Entradas sem identificação<br> de acoplamento:"] 
+    : ["Saídas de complemento <br>acoplados:", "Saídas de complemento sem <br>identificação de acoplamento:"];
+  const colors = !is_component ? ["#0a1a5c", "#6E6E6E", "#BF6A02"] : ["#0a1a5c", "#BF6A02"];
   const total = data.reduce((acc, val) => acc + val, 0);
   
   const centerX = canvas.width / 2;
@@ -75,21 +77,27 @@ function mountCouplingDonutGraph(canva_id, tooltip_id, data) {
   });
 }
 
-function mountCouplingGridGraph(canva_id, info_box_id, data, isComponent = false) {
+function mountCouplingGridGraph(canva_id, info_box_id, data, is_component = false) {
   const canvas = document.getElementById(canva_id);
   const ctx = canvas.getContext('2d');
   const infoBox = document.getElementById(info_box_id);
   const dotRadius = 5;
   const padding = 40;
+  
+  const entries = Object.keys(data).map(String).sort().reverse();
+  const outputs = 
+  !is_component ? [...new Set(
+    Object.values(data)
+      .flatMap(obj => Object.keys(obj).map(String))
+      .filter(key => key !== 'analysed')
+  )].sort():
+  [...new Set( Object.values(data).flatMap(obj => obj['sut_outputs'].map(String)) )].sort();
 
-  const entries = Object.keys(data).map(Number).sort((a, b) => a - b);
-  const outputs = [...new Set(Object.values(data).flatMap(obj => Object.keys(obj).map(Number)))].sort((a, b) => a - b);
-
-  drawGrid(canvas, ctx, entries, outputs, padding, isComponent);
-  drawCouplingPoints(canvas, ctx, infoBox, dotRadius, padding, entries, outputs, data);
+  drawGrid(canvas, ctx, entries, outputs, padding, is_component);
+  drawCouplingPoints(canvas, ctx, infoBox, dotRadius, padding, entries, outputs, data, is_component);
 }
 
-function drawGrid(canvas, ctx, entries, outputs, padding, isComponent) {
+function drawGrid(canvas, ctx, entries, outputs, padding, is_component) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   
   for (let i = 0; i < entries.length; i++) {
@@ -104,7 +112,7 @@ function drawGrid(canvas, ctx, entries, outputs, padding, isComponent) {
     ctx.fillStyle = '#000';
     ctx.fillText(entries[i], padding - 20, y + 3);
     if(i === 0) {
-      ctx.fillText((isComponent ? 'Saídas do Componente' : 'Entradas'), padding - 25, y - 20);
+      ctx.fillText((is_component ? 'Saídas do Componente' : 'Entradas'), padding - 25, y - 20);
     }
   }
 
@@ -118,17 +126,20 @@ function drawGrid(canvas, ctx, entries, outputs, padding, isComponent) {
     ctx.fillStyle = '#000';
     ctx.fillText(outputs[j], x - 5, canvas.height - padding + 20);
     if(j === outputs.length - 1) {
-      ctx.fillText('Saídas', x + 5, canvas.height - padding + 25);
+      ctx.fillText('Saídas', x - 10, canvas.height - padding + 35);
     }
   }
 }
 
-function drawCouplingPoints(canvas, ctx, infoBox, dotRadius, padding, entries, outputs, data) {
+function drawCouplingPoints(canvas, ctx, infoBox, dotRadius, padding, entries, outputs, data, is_component) {
+  console.log(entries);
+  console.log(outputs);
   for (let i = 0; i < entries.length; i++) {
     const entry = entries[i];
     for (let j = 0; j < outputs.length; j++) {
       const output = outputs[j];
-      if (data[entry] && data[entry][output]) {
+      let shouldDraw = !is_component ? data[entry] && data[entry][output] : data[entry] && data[entry]['sut_outputs'].includes(output);
+      if (shouldDraw) {
         let x = padding + j * (canvas.width - padding * 2) / ((outputs.length === 1 ? 2 : outputs.length) - 1);
         let y = padding + i * (canvas.height - padding * 2) / ((entries.length === 1 ? 2 : entries.length) - 1);
 
@@ -144,11 +155,20 @@ function drawCouplingPoints(canvas, ctx, infoBox, dotRadius, padding, entries, o
           if (Math.hypot(mouseX - x, mouseY - y) < dotRadius) {
             infoBox.style.display = 'block';
             infoBox.style.left = `${event.layerX + 10}px`;
-            infoBox.style.top = `${event.layerY+ 10}px`;
+            infoBox.style.top = `${event.layerY + 10}px`;
             infoBox.innerHTML = `
               <strong>Entrada:</strong> ${entry}<br>
               <strong>Saída:</strong> ${output}<br>
-              <strong>Valores:</strong> ${data[entry][output].join(',')}
+              ${
+                !is_component
+                  ? `<strong>Testes de análise: <br></strong> 
+                    ${data[entry][output]
+                      .map(testes => `teste ${testes[0]} <=> teste ${testes[1]}`)
+                      .join('<br>')}`
+                  : data[entry]['forced']
+                  ? '<strong class="fc-error">Valor forçado<br></strong>'
+                  : ''
+              }
             `;
           }
         });
